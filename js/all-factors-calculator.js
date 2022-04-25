cc630827a819604b849f3bb6c8b6e2855c84855c
Chase Martinez <chasemartinez22@gmail.com>
2022-04-24 20:44:45 -0500

    Add: while loop function that follows the same type of calculation, commented out

--- js/all-factors-calculator.js
+++ js/all-factors-calculator.js
@@ -1,9 +1,11 @@
 // The purpose of this function is to determine all the factors
 // of a given number, store the factor values in an array, and
-// return the array of factors.
+// return the array of factors.  A while loop version is also
+// available below the for loop functino version, currently
+// commented out.
 
 function allFactorsCalculator (number){
-    var foundFactors =[];
+    var foundFactors = [];
     if (number < 1){
         return "Invalid value";
     }
@@ -14,3 +16,18 @@ function allFactorsCalculator (number){
     }
     return foundFactors;
 }
+
+// function allFactorsCalculator (number){
+//     var foundFactors = [];
+//     var i = 1;
+//     if(number < 1){
+//         return "Invalid input";
+//     }
+//     while(i <= number){
+//         if(number % i === 0){
+//             foundFactors.push(i);
+//         }
+//         i++;
+//     }
+//     return foundFactors;
+// }
