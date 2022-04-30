5b85dcc4ebfd26599523d474a267a1916a668907
Chase Martinez <chasemartinez22@gmail.com>
2022-04-30 14:13:37 -0500

    Refactored to contain IIFEs

--- js/circle-calculator.js
+++ js/circle-calculator.js
@@ -1,19 +1,22 @@
+(function () {
+
 // this function will calculate the AREA of a circle given a RADIUS
-function circleAreaRadius(radius) {
-    return radius <= 0 ? "Please provide positive radius." : radius * radius * Math.PI;
-}
+    function circleAreaRadius(radius) {
+        return radius <= 0 ? "Please provide positive radius." : radius * radius * Math.PI;
+    }
 
 // this function will calculate the AREA of a circle given a DIAMETER
-function circleAreaDiameter(diameter) {
-    return diameter <= 0 ? "Please provide a positive diameter." : (diameter / 2) * (diameter / 2) * Math.PI;
-}
+    function circleAreaDiameter(diameter) {
+        return diameter <= 0 ? "Please provide a positive diameter." : (diameter / 2) * (diameter / 2) * Math.PI;
+    }
 
 // this function will calculate the CIRCUMFERENCE of a circle given a RADIUS
-function circleCircumferenceRadius(radius){
-    return radius <= 0 ? "Please provide a positive radius." : radius * 2 * Math.PI;
-}
+    function circleCircumferenceRadius(radius) {
+        return radius <= 0 ? "Please provide a positive radius." : radius * 2 * Math.PI;
+    }
 
 // this function will calculate the CIRCUMFERENCE of a circle given a DIAMETER
-function circleCircumferenceDiameter(diameter){
-    return diameter <= 0 ? "Please provide a positive diameter." : diameter * Math.PI;
-}
+    function circleCircumferenceDiameter(diameter) {
+        return diameter <= 0 ? "Please provide a positive diameter." : diameter * Math.PI;
+    }
+})();
--- js/objects-mini-exercises.js
+++ js/objects-mini-exercises.js
@@ -1,86 +1,87 @@
+(function () {
 //Mini Exercise #1
 
-var beverages = [
-    {
-        brandName: 'Dr. Pepper',
-        type: 'Soft Drink',
-        volumeInLiters: 0.354882,
-        priceInCents: .99,
-        expirationDate: 2024,
-        datesOfPreviousSips: ['1/17', '2/6'],
-        isOpen: false,
-    },
-    {
-        brandName: 'Sprite',
-        type: 'Soft Drink',
-        volumeInLiters: 0.354882,
-        priceInCents: .99,
-        expirationDate: 2024,
-        datesOfPreviousSips: ['12/25', '4/9'],
-        isOpen: false,
-    },
-    {
-        brandName: 'Coca Cola',
-        type: 'Soft Drink',
-        volumeInLiters: 0.354882,
-        priceInCents: .99,
-        expirationDate: 2024,
-        datesOfPreviousSips: ['1/13', '2/2'],
-        isOpen: false,
-    },
-    {
-        brandName: 'Gatorade',
-        type: 'Sports Drink',
-        volumeInLiters: 0.591471,
-        priceInCents: 1.79,
-        expirationDate: 2023,
-        datesOfPreviousSips: ['4/17', '4/18'],
-        isOpen: true,
-    },
-    {
-        brandName: 'Root Beer',
-        type: 'Soft Drink',
-        volumeInLiters: 0.354882,
-        priceInCents: .99,
-        expirationDate: 2024,
-        datesOfPreviousSips: ['11/20', '2/28'],
-        isOpen: false,
-    },
-    {
-        brandName: 'Big Red',
-    }
-];
+    var beverages = [
+        {
+            brandName: 'Dr. Pepper',
+            type: 'Soft Drink',
+            volumeInLiters: 0.354882,
+            priceInCents: .99,
+            expirationDate: 2024,
+            datesOfPreviousSips: ['1/17', '2/6'],
+            isOpen: false,
+        },
+        {
+            brandName: 'Sprite',
+            type: 'Soft Drink',
+            volumeInLiters: 0.354882,
+            priceInCents: .99,
+            expirationDate: 2024,
+            datesOfPreviousSips: ['12/25', '4/9'],
+            isOpen: false,
+        },
+        {
+            brandName: 'Coca Cola',
+            type: 'Soft Drink',
+            volumeInLiters: 0.354882,
+            priceInCents: .99,
+            expirationDate: 2024,
+            datesOfPreviousSips: ['1/13', '2/2'],
+            isOpen: false,
+        },
+        {
+            brandName: 'Gatorade',
+            type: 'Sports Drink',
+            volumeInLiters: 0.591471,
+            priceInCents: 1.79,
+            expirationDate: 2023,
+            datesOfPreviousSips: ['4/17', '4/18'],
+            isOpen: true,
+        },
+        {
+            brandName: 'Root Beer',
+            type: 'Soft Drink',
+            volumeInLiters: 0.354882,
+            priceInCents: .99,
+            expirationDate: 2024,
+            datesOfPreviousSips: ['11/20', '2/28'],
+            isOpen: false,
+        },
+        {
+            brandName: 'Big Red',
+        }
+    ];
 
-beverages[5].type = 'Soft Drink';
-beverages[5].volumeInLiters = 0.354882;
-beverages[5].priceInCents = .99;
-beverages[5].expirationDate = 2024;
-beverages[5].datesOfPreviousSips = ['12/25', '3/6'];
-beverages[5].isOpen = false;
+    beverages[5].type = 'Soft Drink';
+    beverages[5].volumeInLiters = 0.354882;
+    beverages[5].priceInCents = .99;
+    beverages[5].expirationDate = 2024;
+    beverages[5].datesOfPreviousSips = ['12/25', '3/6'];
+    beverages[5].isOpen = false;
 
 //Mini Exercise #2
-var users = [
-    {
-        givenName: 'Sam',
-        age: 21
-    },
-    {
-        givenName: 'Cathy',
-        age:34
-    },
-    {
-        givenName: 'Karen',
-        age: 43,
-    }
-];
-/**
+    var users = [
+        {
+            givenName: 'Sam',
+            age: 21
+        },
+        {
+            givenName: 'Cathy',
+            age: 34
+        },
+        {
+            givenName: 'Karen',
+            age: 43,
+        }
+    ];
+    /**
      1. Log the names of all users in a single console log separated by spaces.
-    output = "Sam Cathy Karen"
-*/
+     output = "Sam Cathy Karen"
+     */
 
 // console.log(users[0].givenName + " " + users[1].givenName + " " + users[2].givenName);
 
-/**  2. Change the names of all users to "John Doe"*/
+    /**  2. Change the names of all users to "John Doe"*/
 
 // users[0].givenName = "John Doe";
 // users[1].givenName = "John Doe";
@@ -96,18 +97,19 @@ var users = [
 //  Can you accomplish each step using iteration
 
 // #1
-var usersNames = '';
-users.forEach(function(user){
-    usersNames += user.givenName + " ";
-});
-console.log(usersNames.trim());
+    var usersNames = '';
+    users.forEach(function (user) {
+        usersNames += user.givenName + " ";
+    });
+    console.log(usersNames.trim());
 
 // #2
-users.forEach(function(user) {
-    user.givenName = "John Doe";
-});
+    users.forEach(function (user) {
+        user.givenName = "John Doe";
+    });
 
 // #3
-users.forEach(function(user){
-    user.age++;
-});
+    users.forEach(function (user) {
+        user.age++;
+    });
+})();
