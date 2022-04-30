2e8b6d03f5645f91e0f8a9209e2951d07a05794f
Chase Martinez <chasemartinez22@gmail.com>
2022-04-29 21:05:04 -0500

    feat: functions that calculate circle area and circumferene using either a radius or a diameter

---
+++ js/area-calculator.js
@@ -0,0 +1,19 @@
+// this function that will calculate the AREA of a circle given a RADIUS
+function circleAreaRadius(radius) {
+    return radius <= 0 ? "Please provide positive radius." : radius * radius * Math.PI;
+}
+
+// this function will calculate the AREA of a circle given a DIAMETER
+function circleAreaDiameter(diameter) {
+    return diameter <= 0 ? "Please provide a positive diameter." : (diameter / 2) * (diameter / 2) * Math.PI;
+}
+
+// this function will calculate the CIRCUMFERENCE of a circle given a RADIUS
+function circleCircumferenceRadius(radius){
+    return radius <= 0 ? "Please provide a positive radius." : radius * 2 * Math.PI;
+}
+
+// this function will calculate the CIRCUMFERENCE of a circle given a DIAMETER
+function circleCircumferenceDiameter(diameter){
+    return diameter <= 0 ? "Please provide a positive diameter." : diameter * Math.PI;
+}
