1729018cd784ae70b80df64fc5d2ee3585632a0b
Chase Martinez <chasemartinez22@gmail.com>
2022-05-04 18:41:44 -0500

    Refactored to properly choose items in stock

--- js/objects-mini-exercise-2.js
+++ js/objects-mini-exercise-2.js
@@ -2,38 +2,38 @@
 //
 // Consider an array of product objects:
 
-    const products = [
-        {
-            name: 'Hammer',
-            priceInCents: 4500,
-            isInStock: true
-        },
-        {
-            name: 'Computer',
-            priceInCents: 45000,
-            isInStock: true
-        },
-        {
-            name: 'Water Bottle',
-            priceInCents: 3300,
-            isInStock: true
-        },
-        {
-            name: 'Car',
-            priceInCents: 990000,
-            isInStock: true
-        },
-        {
-            name: 'Cup',
-            priceInCents: 230,
-            isInStock: false
-        },
-        {
-            name: 'Book',
-            priceInCents: 540,
-            isInStock: false
-        },
-    ];
+const products = [
+    {
+        name: 'Hammer',
+        priceInCents: 4500,
+        isInStock: true
+    },
+    {
+        name: 'Computer',
+        priceInCents: 45000,
+        isInStock: true
+    },
+    {
+        name: 'Water Bottle',
+        priceInCents: 3300,
+        isInStock: true
+    },
+    {
+        name: 'Car',
+        priceInCents: 990000,
+        isInStock: true
+    },
+    {
+        name: 'Cup',
+        priceInCents: 230,
+        isInStock: false
+    },
+    {
+        name: 'Book',
+        priceInCents: 540,
+        isInStock: false
+    },
+];
 // Create the following functions that take in an array of product objects and return various values:
 //
 // returnMostExpensiveProductObjectInStock(products)
@@ -41,37 +41,42 @@
 // returnAveragePriceOfAllProductsInStock(products)
 // returnProductObjectsNotInStock(products)
 
-function returnMostExpensiveProductObjectInStock(products){
+function returnMostExpensiveProductObjectInStock(products) {
     var mostExpensive = 0;
-    for(var i = 0;i < products.length;i++){
-        if(products[i].priceInCents > mostExpensive){
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].priceInCents > mostExpensive && products[i].isInStock === true) {
             mostExpensive = products[i].priceInCents;
         }
     }
     return mostExpensive;
 }
+
 function returnLeastExpensiveProductObjectInStock(products) {
     var leastExpensive = Number.MAX_VALUE;
-    for(var i = 0;i < products.length;i++){
-        if(products[i].priceInCents < leastExpensive){
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].priceInCents < leastExpensive && products[i].isInStock === true) {
             leastExpensive = products[i].priceInCents;
         }
     }
     return leastExpensive;
 }
-function returnAveragePriceOfAllProductsInStock(products){
+
+function returnAveragePriceOfAllProductsInStock(products) {
     var averagePrice = 0;
     var counter = 0;
-    for(var i = 0;i < products.length;i++){
-        averagePrice += products[i].priceInCents;
-        counter++;
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].isInStock === true) {
+            averagePrice += products[i].priceInCents;
+            counter++;
+        }
     }
-    return averagePrice/counter;
+    return averagePrice / counter;
 }
-function returnProductObjectsNotInStock(products){
-    var notInStock =[];
-    for(var i = 0;i < products.length;i++){
-        if(products[i].isInStock === false){
+
+function returnProductObjectsNotInStock(products) {
+    var notInStock = [];
+    for (var i = 0; i < products.length; i++) {
+        if (products[i].isInStock === false) {
             notInStock.push(products[i].name)
         }
     }
