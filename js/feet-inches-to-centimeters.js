acbafee92f2fd88e26bd35a4fe2a8f8984cd3dc5
Chase Martinez <chasemartinez22@gmail.com>
2022-05-14 14:49:54 -0500

    Refactored to return cm to two decimal places Alsoo refactored conditionals

--- js/feet-inches-to-centimeters.js
+++ js/feet-inches-to-centimeters.js
@@ -1,20 +1,22 @@
 (function () {
     "use strict";
 
-    // Given integers of feet and inches, this function will convert feet and
-    // inches to metric measurements.
-    function calcFeetAndInchesToMetric(numberOfFeet, numberOfInches) {
-        let totalInches = (Math.floor(parseFloat(numberOfFeet) * 12)) + parseFloat(numberOfInches);
-        let totalCentimeters = totalInches * 2.54;
-        let meters = Math.floor(totalCentimeters / 100);
-        return meters + "m " + (totalCentimeters %= 100) + "cm";
+    // Given numbers of feet and inches, this function will convert feet and
+    // inches to metric measurements.  Formatting cm to two decimal places.
+    function feetAndInchesToMetric(numberOfFeet, numberOfInches) {
+        if (!isNaN(parseFloat(numberOfFeet)) && !isNaN(parseFloat(numberOfInches))) {
+            let totalCentimeters = ((Math.floor(parseFloat(numberOfFeet) * 12)) + parseFloat(numberOfInches)) * 2.54;
+            let meters = Math.floor(totalCentimeters / 100);
+            return meters + "m " + (totalCentimeters %= 100).toFixed(2) + "cm";
+        } else {
+            return "Please use valid numbers"
+        }
     }
 
 
-    // Given only one integer of inches, this function will use the previous function
+    // Given only one number for inches, this function will use the previous function
     // to convert inches to the metric system.
     function inchesToMetric(numberOfInches) {
-        return (numberOfInches >= 0 && !isNaN(parseFloat(numberOfInches)))
-            ? calcFeetAndInchesToMetric((Math.floor(parseFloat(numberOfInches) / 12)), parseFloat(numberOfInches) % 12) : "Please use a valid number"
+        return feetAndInchesToMetric((Math.floor(numberOfInches / 12)), numberOfInches % 12);
     }
 }());
