f61971538ccbd00fae1261dbbb4c5d6a885c5671
Chase Martinez <chasemartinez22@gmail.com>
2022-05-15 14:22:43 -0500

    Complete for loop, while loop for each loop currently being written

--- js/first-last-digit-sum.js
+++ js/first-last-digit-sum.js
@@ -14,7 +14,7 @@
             }
             return sum;
         } else {
-            return "Please use an integer(non-decimal) value"
+            return "Please use an integer(non-decimal) value";
         }
     }
 
@@ -38,8 +38,22 @@
     }
 
     // This function accepts an integer and returns the sum of the first
-    // and last digit, FOR EACH LOOP version.  If a number is a single digit
-    // such as 5 then the sum will equal 10, since 5 is technically the
-    // first and last digit.
+    // and last digit, FOR EACH LOOP version.  If a number is a single
+    // digit such as 5 then the sum will equal 10, since 5 is technically
+    // the first and last digit. Because this type of loop requires an
+    // array, the number/string is converted to an array.  There's a much
+    // simpler way to handle an array and add first/last digit (such as:
+    // return array[0] + array[array.length -1]...) but, the purpose of
+    // this is to demonstrate the understanding of different types of loops.
 
+    function forEachLoopFirstAndLastDigitSum(num) {
+        if (parseFloat(num) === Math.floor(parseFloat(num))) {
+            let sum = 0;
+            let numbers = num.toString().split("");
+            //  return numbers[0] + numbers[numbers.length - 1];
+            numbers.forEach((function (number){
+                if(number === numbers[0] || number === numbers[numbers.length -1])
+            }))
+        }
+    }
 }());
