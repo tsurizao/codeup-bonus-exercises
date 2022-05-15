81a4595ba70e35acd4f12c2bd573f23ab825d34e
Chase Martinez <chasemartinez22@gmail.com>
2022-05-15 15:06:23 -0500

    Updating with current code

--- js/first-last-digit-sum.js
+++ js/first-last-digit-sum.js
@@ -1,6 +1,17 @@
 (function () {
     "use strict"
 
+    // There are MUCH easier ways to handle adding the first/last digit
+    // together (such as:
+
+    function easyFirstAndLastDigitSumFunction(num) {
+        let array = num.toString().split("");
+        return array[0] + array[array.length - 1];
+    }
+
+    // ) The purpose of this is to demonstrate the understanding of
+    // different types of loops.
+
     // This function accepts an integer and returns the sum of the first
     // and last digit, FOR LOOP version.  If a number is a single digit
     // such as 5 then the sum will equal 10, since 5 is technically the
@@ -41,19 +52,22 @@
     // and last digit, FOR EACH LOOP version.  If a number is a single
     // digit such as 5 then the sum will equal 10, since 5 is technically
     // the first and last digit. Because this type of loop requires an
-    // array, the number/string is converted to an array.  There's a much
-    // simpler way to handle an array and add first/last digit (such as:
-    // return array[0] + array[array.length -1]...) but, the purpose of
-    // this is to demonstrate the understanding of different types of loops.
+    // array, the number/string is converted to an array.
 
     function forEachLoopFirstAndLastDigitSum(num) {
         if (parseFloat(num) === Math.floor(parseFloat(num))) {
-            let sum = 0;
             let numbers = num.toString().split("");
-            //  return numbers[0] + numbers[numbers.length - 1];
-            numbers.forEach((function (number){
-                if(number === numbers[0] || number === numbers[numbers.length -1])
-            }))
+            //  return numbers[0] + numbers[numbers.length - 1];  easier solution
+            let newNumbers = [0, 0];
+            numbers.forEach((function (number) {
+                if (newNumbers[0] === 0) {
+                    newNumbers[0] = Number.number;
+                }
+                if (Number.number === num % 10) {
+                    newNumbers[newNumbers.length - 1] = Number.number;
+                }
+            }));
+            return newNumbers[0] + newNumbers[1];
         }
     }
 }());
