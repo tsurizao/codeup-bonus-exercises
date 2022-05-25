4c5fe41b3a9099c226b633108250fd3a39ccb06d
Chase Martinez <chasemartinez22@gmail.com>
2022-05-25 09:08:36 -0500

    add completed mini exercise

---
+++ js/es6-mini-exercise.js
@@ -0,0 +1,5 @@
+// TODO: Write an arrow function named "highFive" which accepts a string and alerts the user's input, asking for a high five.
+// Example: highFive("Go") returns "Go, high five!"
+
+let highFive = string => string + ", high five!";
+highFive("Go");
