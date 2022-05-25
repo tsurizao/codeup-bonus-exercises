37a275ccfed85d7df51e154db688795ce1dcf22f
Chase Martinez <chasemartinez22@gmail.com>
2022-05-25 09:13:48 -0500

    Add arrow function with alert instead of console.log

--- js/es6-mini-exercise.js
+++ js/es6-mini-exercise.js
@@ -1,5 +1,6 @@
 // TODO: Write an arrow function named "highFive" which accepts a string and alerts the user's input, asking for a high five.
 // Example: highFive("Go") returns "Go, high five!"
 
-let highFive = string => string + ", high five!";
+// const highFive = string => `${string}, high five!`;
+const highFive = str => alert(`${str}, high five!`)
 highFive("Go");
